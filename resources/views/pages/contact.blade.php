adeb1552ccd7d91a2a8fe7aaed1433d3d13c91ac
yayan <admiral0008@gmail.com>
2015-03-25 15:47:43 +0700

    commot laravel pertama

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>blade</title>
+</head>
+<body>
+
+<a href="ha">ahayyyy</a>
+<p>baruuu contak</p>
+</body>
+</html>
---
+++ resources/views/pages/yayan.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>yayan</title>
+</head>
+<body>
+<h1>HALOOOOO DI SIMPAN DI PAGES RESOURCE </h1>
+</body>
+</html>
